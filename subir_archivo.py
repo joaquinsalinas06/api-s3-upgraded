33f66deb9cff6d132db563020f8936e07bdcf6fb
joaquinsalinas06 <103973127+joaquinsalinas06@users.noreply.github.com>
2024-11-03 17:46:36 -0500

    Update subir_archivo.py

--- subir_archivo.py
+++ subir_archivo.py
@@ -1,13 +1,13 @@
 import boto3
+import base64
 
 def lambda_handler(event, context):
     bucket_name = event['body']['bucket']
-    directory = event['body']['directory']
     file_name = event['body']['file_name']
+    file_content = base64.b64decode(event['body']['file_content'])
     
     s3 = boto3.client('s3')
-    with open(directory, 'rb') as fileu:
-        s3.upload_fileobj(fileu, bucket_name, file_name)
+    s3.put_object(Bucket=bucket_name, Key=file_name, Body=file_content)
     
     return {
         'statusCode': 200,
