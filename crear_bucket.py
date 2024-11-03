14cf3d0b8e2e81f1557c5d002ff098e549580d31
joaquinsalinas06 <103973127+joaquinsalinas06@users.noreply.github.com>
2024-11-03 17:52:57 -0500

    Update crear_bucket.py

--- crear_bucket.py
+++ crear_bucket.py
@@ -2,9 +2,12 @@ import boto3
 
 def lambda_handler(event, context):
     bucket_name = event['body']['bucket']
+
     
     s3 = boto3.client('s3')
     response = s3.create_bucket(Bucket=bucket_name)
+
+
     
     return {
         'statusCode': 200,
